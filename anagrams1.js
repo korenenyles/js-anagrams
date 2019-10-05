3b3e44d60e3b73505f14a1ecd873b6b9a5ba4650
Koren Nyles <korenenyles@gmail.com>
2019-10-05 09:48:25 -0400

    Fixed functions

--- anagrams1.js
+++ anagrams1.js
@@ -1,28 +1,33 @@
 const button = document.getElementById("findButton");
-let wordsSplit = words.split(", ");
+button.onclick = function() {
+  // your code goes here
+  let typedText = document.getElementById("input").value;
+  let words = typedText.split(", ");
+  console.log(getAnagramsOf(typedText));
+  let result = getAnagramsOf(typedText);
+  let newDiv = document.createElement("div");
+  newDiv.innerHTML = result.join(", ");
+  document.getElementById("results").appendChild(newDiv);
+};
 
-function getAnagramsOf(typedText){
-    for( let i = 0; i < wordsSplit.length; i++){
-    let alphabetical = wordsSplit.split("").sort().join("");
-    
-        
+function getAnagramsOf(myWord) {
+  let anagramArray = [];
+  let newWord = alphabetize(myWord);
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let currentWord = alphabetize(word);
+    if (currentWord === newWord) {
+      anagramArray.push(word);
     }
-    return words;
+  }
+  return anagramArray;
 }
-
-button.onclick = function () {
-    // your code goes here
-
-}
-
-
-
-    let typedText = document.getElementById("input").value;
-    getAnagramsOf(typedText);
-
-
-
 
 function alphabetize(a) {
-    return a.toLowerCase().split("").sort().join("").trim();
+  return a
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("")
+    .trim();
 }
